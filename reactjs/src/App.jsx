dfe7a314a8851ec940f0d81abac0177d8a810871
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-09-06 00:33:42 +0800

    hello world!

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
 
   return (
-    <>
-    </>
+    <div class="flex justify-center items-center min-h-screen">
+        <h1 class="text-4xl font-bold animate-bounce">Hello, World!</h1>
+    </div>
   )
 }
 
